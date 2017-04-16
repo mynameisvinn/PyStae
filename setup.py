865fa4940e9694f2692d594404b6ab964dd0dc66
mynameisvinn <vin.tang@gmail.com>
2017-04-16 11:59:55 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from distutils.core import setup
 
 setup(
-    name="vin's lirbary",
+    name="pystae",
     version="0.1.0",
-    packages=["vin_library"], # source code folder
+    packages=["pystae"], # source code folder
 
     # Include additional files into the package
     include_package_data=True,
 
     # Details
-    url="http://pypi.python.org/pypi/MyApplication_v010/",
+    # url="http://pypi.python.org/pypi/MyApplication_v010/",
 
     # Dependent packages (distributions)
     install_requires=[
-        "numpy",
+        "pandas", "requests"
     ],
 )
