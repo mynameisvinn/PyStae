bab7bfd633f9f78158a2ede22106ac702971732a
mynameisvinn <vin.tang@gmail.com>
2017-04-16 12:27:46 -0400

    decorate w pandas df

--- pystae/pystae.py
+++ pystae/pystae.py
@@ -1,19 +1,27 @@
-import pandas as pd 
+import pandas as pd
 import requests
- 
+
+def to_dataframe(func):
+    def wrapper(municipalityId):
+        return pd.DataFrame(func(municipalityId))
+    return wrapper
+
+
+@to_dataframe
 def fetch_businesses(municipalityId):
     try:
     	identifier = 'https://municipal.systems/v1/municipalities/' + municipalityId + '/businesses'
         r = requests.get(identifier).json()
-        return pd.DataFrame(r['results'])
+        return r['results']
     except ValueError:
         print "invalid municipalityId"
 
 
+@to_dataframe
 def fetch_building_permits(municipalityId):
     try:
     	identifier = 'https://municipal.systems/v1/municipalities/' + municipalityId + '/building_permits'
         r = requests.get(identifier).json()
-        return pd.DataFrame(r['results'])
+        return r['results']
     except ValueError:
         print "invalid municipalityId"
