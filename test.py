ba6405a53ddb2884a18b9521b6be7ad9802e7196
mynameisvinn <vin.tang@gmail.com>
2017-04-16 16:55:38 -0400

    test response code

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import unittest
+import requests
+
+MUNICIPALITY_ID = 'https://municipal.systems/v1/municipalities/jers-nj/trips'
 
 class InitializationTests(unittest.TestCase):
 
@@ -10,6 +13,10 @@ class InitializationTests(unittest.TestCase):
             import pystae
         except ImportError:
             self.fail("Was not able to import the emailgraph")
+
+    def test_response(self):
+        r = requests.get(MUNICIPALITY_ID)
+        self.assertEqual(r.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
