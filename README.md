02940b66aab5d5a37ce352120d9a448ae9b57736
mynameisvinn <vin.tang@gmail.com>
2017-04-16 11:44:41 -0400

    hello

---
+++ README.md
@@ -0,0 +1,15 @@
+# PyStae
+
+a tool for downloading datasets in a python pandas friendly format from stae-powered open data portals.
+
+users can provide a 'stae' dataset resource url and retrieve a pandas dataframe.
+
+## Installation
+``` 
+git clone github.com/mynameisvinn/PyStae
+cd PyStae
+python setup.py install
+```
+
+## Examples
+TODO
---
+++ test.py
@@ -0,0 +1,27 @@
+import unittest
+
+class InitializationTests(unittest.TestCase):
+
+    def test_initialization(self):
+        """
+        Check the test suite runs by affirming 2+2=4
+        """
+        self.assertEqual(2+2, 4)
+
+    def test_import(self):
+        """
+        Ensure the test suite can import our module
+        """
+        try:
+            import pymem
+        except ImportError:
+            self.fail("Was not able to import the emailgraph")
+
+    def test_string(self):
+    	# from pymem import deep_getsizeof
+    	# x = '1234567'
+    	# assert (deep_getsizeof(x, set()) == 44)
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
