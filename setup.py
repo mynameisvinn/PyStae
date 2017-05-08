ee06c1f58a4c8eae037deebc3d99dd55823c2d6d
mynameisvinn <vin.tang@gmail.com>
2017-05-07 20:48:46 -0400

    reorganize folders

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="pystae",
-    author="vin tang",
-    author_email="vin.tang@gmail.com",
-    version="0.1.0",
-    packages=["pystae"], # source code folder
-    url='https://github.com/mynameisvinn/pystae',
-
-    # Dependent packages (distributions)
-    install_requires=[
-        "pandas", "requests"
-    ],
+    packages=["pystae"],
+    install_requires=["pandas", "requests", "joblib"],
+    test_suite='tests'
 )
--- tests/test_pystae.py
+++ tests/test_pystae.py
@@ -6,14 +6,14 @@ MUNICIPALITY_ID = 'https://municipal.systems/v1/municipalities/jers-nj/trips'
 class InitializationTests(unittest.TestCase):
 
     def test_initialization(self):
-        self.assertEqual(2+2, 4)
+        pass
 
     def test_import(self):
         try:
             import pystae
         except ImportError:
-            self.fail("Was not able to import the emailgraph")
-
+            self.fail("Failed to import pystae")
+    
     def test_response(self):
         r = requests.get(MUNICIPALITY_ID)
         self.assertEqual(r.status_code, 200)
